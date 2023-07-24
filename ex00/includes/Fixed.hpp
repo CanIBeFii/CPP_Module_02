dd5e6941f8057b734c2b6b2ef2ebe5376a53224e
CanIBeFii <filipe250502@gmail.com>
2023-07-24 16:40:56 +0100

    ex00: simple Fixed point done

---
+++ ex00/includes/Fixed.hpp
@@ -0,0 +1,23 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	public:
+		Fixed( void );
+		Fixed( int const value );
+		Fixed
+		Fixed( Fixed &copy );
+		Fixed& operator=( Fixed const &copy );
+		~Fixed( void );
+	
+		int		getRawBits( void ) const ;
+		void	setRawBits( int const raw );
+
+	private:
+		int					_rawValue;
+		static const int	_fractionalBits = 8;
+};
+
+#endif 
