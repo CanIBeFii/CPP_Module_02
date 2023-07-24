afc1cf11019062b56d1d6266a488994ef7037fe8
CanIBeFii <filipe250502@gmail.com>
2023-07-24 16:41:29 +0100

    ex01: add base files

---
+++ ex01/sources/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ) : _rawValue( 0 ) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( Fixed &copy ) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+	//_rawValue = copy.getRawBits();
+}
+
+Fixed & Fixed::operator = ( Fixed const &copy ) {
+	std::cout << "Copy assigment operator called" <<std::endl;
+
+	if ( this != &copy ) {
+		this->_rawValue = copy.getRawBits();
+	}
+	return ( *this );
+}
+
+Fixed::~Fixed( void ) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	
+	return ( _rawValue );
+}
+
+void	Fixed::setRawBits( int const raw ) {
+	std::cout << "setRwBits member function called" << std::endl;
+
+	_rawValue = raw;
+}
