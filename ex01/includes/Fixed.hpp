b98a8475e404b84c52d4c486d68de2ba585173a2
CanIBeFii <filipe250502@gmail.com>
2023-08-30 16:39:58 +0100

    add new function to b apllied

--- ex01/includes/Fixed.hpp
+++ ex01/includes/Fixed.hpp
@@ -6,10 +6,15 @@
 class Fixed {
 	public:
 		Fixed( void );
+		Fixed( const int intValue );
+		Fixed( const float floatValue );
 		Fixed( Fixed &copy );
 		Fixed& operator=( Fixed const &copy );
 		~Fixed( void );
 	
+		float	toFloat( void );
+		int		toInt( void );
+
 		int		getRawBits( void ) const ;
 		void	setRawBits( int const raw );
 
@@ -17,5 +22,7 @@ class Fixed {
 		int					_rawValue;
 		static const int	_fractionalBits = 8;
 };
+
+std::ostream & operator << (std::ostream &out, Fixed const &f);
 
 #endif 
