acc988f6eb9cc02c0db772f64574e9cf7049812d
CanIBeFii <filipe250502@gmail.com>
2023-09-13 18:02:26 +0100

    add Point class and calculations of area of triangule

---
+++ ex03/sources/bsp.cpp
@@ -0,0 +1,13 @@
+#include "Point.hpp"
+
+float	absolute( float number ) {
+	return ( number < 0 ? number * -1 : number );
+}
+
+float	trianguleArea( Point const a, Point const b, Point const c ) {
+	return ( 0.5 * abosolute());
+}
+
+bool	bsp( Point const a, Point const b, Point const c, Point const point ) {
+
+}
