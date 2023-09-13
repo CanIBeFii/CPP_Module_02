915c8797a0a2443e42256e9ef7d490eba9c7872f
CanIBeFii <filipe250502@gmail.com>
2023-09-13 14:38:42 +0100

    delete unused files and update Fixed operators

--- ex02/sources/Fixed.cpp
+++ ex02/sources/Fixed.cpp
@@ -1,29 +1,29 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed( void ) : _rawValue( 0 ) {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed( const int intValue) {
-	std::cout << "Int value constructor called" << std::endl;
+	// std::cout << "Int value constructor called" << std::endl;
 
 	_rawValue = intValue << _fractionalBits;
 }
 
 Fixed::Fixed( const float floatValue ) {
-	std::cout << "Float value constructor called" << std::endl;
+	// std::cout << "Float value constructor called" << std::endl;
 
 	_rawValue = round(floatValue * (1 << _fractionalBits));
 }
 
 Fixed::Fixed( const Fixed &copy ) {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
 	//_rawValue = copy.getRawBits();
 }
 
 Fixed & Fixed::operator = ( Fixed const &copy ) {
-	std::cout << "Copy assigment operator called" <<std::endl;
+	// std::cout << "Copy assigment operator called" <<std::endl;
 
 	if ( this != &copy ) {
 		this->_rawValue = copy.getRawBits();
@@ -32,7 +32,7 @@ Fixed & Fixed::operator = ( Fixed const &copy ) {
 }
 
 Fixed::~Fixed( void ) {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
 
 int		Fixed::toInt( void ) const {
@@ -92,77 +92,67 @@ int		Fixed::operator != ( const Fixed &f ) const {
 // Arithmetic Operators
 
 Fixed	Fixed::operator + ( const Fixed &f ) const {
-	Fixed	newFixed( this->toFloat() + f.toFloat() );
+	Fixed	newFixed;
 
+	newFixed.setRawBits( getRawBits() + f.getRawBits() );
 	return ( newFixed );
 }
 
 Fixed	Fixed::operator - ( const Fixed &f ) const {
-	Fixed newFixed( this->toFloat() - f.toFloat() );
+	Fixed	newFixed;
 
+	newFixed.setRawBits( getRawBits() - f.getRawBits() );
 	return ( newFixed );
 }
 
 Fixed	Fixed::operator * ( const Fixed &f ) const {
-	Fixed	newFixed( this->toFloat() * f.toFloat() );
+	Fixed	newFixed;
 
+	newFixed.setRawBits( getRawBits() * f.getRawBits() );
+	newFixed.setRawBits( newFixed.getRawBits() >> _fractionalBits );
 	return ( newFixed );
 }
 
 Fixed	Fixed::operator / ( const Fixed &f ) const {
-	Fixed	newFixed( this->toFloat() / f.toFloat() );
+	Fixed	newFixed;
 
+	newFixed.setRawBits( getRawBits() << _fractionalBits );
+	newFixed.setRawBits( newFixed.getRawBits() / f.getRawBits() );
 	return ( newFixed );
 }
 
 // Pre-Increment/Pre-Decrement Operators
 
 Fixed	Fixed::operator ++ ( void ) {
-	Fixed	newFixed( *this );
-	int		newRawBits;
-
-	newRawBits = newFixed.getRawBits();
-	newRawBits++;
-	newFixed.setRawBits( newRawBits );
-	return ( newFixed );
+	setRawBits( getRawBits() + 1 );
+	return ( *this );
 }
 
 Fixed	Fixed::operator -- ( void ) {
-	Fixed	newFixed( *this );
-	int		newRawBits;
-
-	newRawBits = newFixed.getRawBits();
-	newRawBits++;
-	newFixed.setRawBits( newRawBits );
-	return ( newFixed );
+	setRawBits( getRawBits() - 1 );
+	return ( *this );
 }
 
 // Pos-Increment/Pos-Decrement Operators
 
 Fixed	Fixed::operator ++ ( int ) {
 	Fixed	newFixed( *this );
-	int		newRawBits;
 
-	newRawBits = newFixed.getRawBits();
-	++newRawBits;
-	newFixed.setRawBits( newRawBits );
+	++*this;
 	return ( newFixed );
 }
 
 Fixed	Fixed::operator -- ( int ) {
 	Fixed	newFixed( *this );
-	int		newRawBits;
-
-	newRawBits = newFixed.getRawBits();
-	--newRawBits;
-	newFixed.setRawBits( newRawBits );
+	
+	--*this;
 	return ( newFixed );
 }
 
 // Min and Max Functions
 
 Fixed	&Fixed::min( Fixed &f1, Fixed &f2 ) {
-	return ( f1 < f2 ? f1 : f2);
+	return ( f1 < f2 ? f1 : f2 );
 }
 
 Fixed	&Fixed::min( const Fixed &f1, const Fixed &f2 ) {
@@ -170,7 +160,7 @@ Fixed	&Fixed::min( const Fixed &f1, const Fixed &f2 ) {
 }
 
 Fixed	&Fixed::max( Fixed &f1, Fixed &f2 ) {
-	return ( f1 < f2 ? f1 : f2);
+	return ( f1 > f2 ? f1 : f2 );
 }
 
 Fixed	&Fixed::max( const Fixed &f1, const Fixed &f2 ) {
